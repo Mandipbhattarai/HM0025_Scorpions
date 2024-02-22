00eb5b67de7d8fe9f4069f0b37de562cd6f7f75c
Mandip Bhattarai <97532160+Mandipbhattarai@users.noreply.github.com>
2024-02-22 14:22:58 +0530

    Add files via upload

--- js/initializer.js
+++ js/initializer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -9,15 +8,3 @@ const userPhoto = urlParams.get('userPhoto');
 // Update HTML elements with user credentials
 document.getElementById("name").innerText = "Hi, "+userName || "Unknown User";
 
-=======
-
-
-const urlParams = new URLSearchParams(window.location.search);
-const userName = urlParams.get('userName');
-const userEmail = urlParams.get('userEmail');
-const userPhoto = urlParams.get('userPhoto');
-
-// Update HTML elements with user credentials
-document.getElementById("name").innerText = "Hi, "+userName || "Unknown User";
-
->>>>>>> 66974c3d37e16aebcd924a1cfeaf92c0d94cf397
--- js/piechart.js
+++ js/piechart.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'pie',
@@ -24,24 +23,3 @@ var myChart = new Chart(ctx, {
         },
     }
 });
-=======
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ["Stocks (Equities) - Including SIPs", "Bonds and Fixed Deposits (FDs)", "Real Estate", "Cash and Cash Equivalents", "Gold Investments", "Alternative Investments", "Retirement Savings"],
-        datasets: [{
-            backgroundColor: [
-                "#2ecc71",
-                "#3498db",
-                "#e74c3c",
-                "#f1c40f",
-                "#9b59b6",
-                "#34495e",
-                "#16a085"
-            ],
-            data: [40, 20, 10, 5, 5, 5, 15]
-        }]
-    }
-});
->>>>>>> 66974c3d37e16aebcd924a1cfeaf92c0d94cf397
